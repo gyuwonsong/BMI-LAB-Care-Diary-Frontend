9b38537e167db40ba43425d3b790aa7e4b4fec86
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2025-12-10 06:15:42 +0900

    feat: 날짜 및 요일 반환 함수 선언

---
+++ src/utils/date.ts
@@ -0,0 +1,18 @@
+export const formatKoreanDate = (date: Date) => {
+  const days = [
+    "일요일",
+    "월요일",
+    "화요일",
+    "수요일",
+    "목요일",
+    "금요일",
+    "토요일",
+  ];
+
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const weekday = days[date.getDay()];
+
+  return `${year}년 ${month}월 ${day}일 ${weekday}`;
+};
